69ee6158b7db96388f32c07cf567f505e581fedf
wwsasuke <390290647@qq.com>
2022-08-23 07:06:41 +0800

    fix mysql no connection

--- src/config/envs/dev.js
+++ src/config/envs/dev.js
@@ -6,7 +6,7 @@
 module.exports = {
     // mysql 连接配置
     mysqlConf: {
-        host: 'localhost',
+        host: '127.0.0.1',
         user: 'root',
         password: 'Mysql_2019',
         port: '3306',
@@ -15,7 +15,7 @@ module.exports = {
 
     // mongodb 连接配置
     mongodbConf: {
-        host: 'localhost',
+        host: '127.0.0.1',
         port: '27017',
         dbName: 'imooc_lego_course',
     },
